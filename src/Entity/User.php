ade2cc1abc170ba0fb8769ab9a5ef24ba4f07b08
Unknown <alexandreplanque@hotmail.fr>
2018-10-12 00:41:43 +0200

    Connexion & deconnexion DONE

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -107,26 +107,6 @@ class User implements UserInterface
         return $this;
     }
 
-    public function eraseCredentials() {
-        
-    }
-
-    public function getPassword(): string {
-        
-    }
-
-    public function getRoles() {
-        
-    }
-
-    public function getSalt() {
-        
-    }
-
-    public function getUsername(): string {
-        
-    }
-
     public function getRole(): ?Role
     {
         return $this->role;
@@ -169,6 +149,67 @@ class User implements UserInterface
 
         return $this;
     }
+    /**
+     * Returns the roles granted to the user.
+     *
+     * <code>
+     * public function getRoles()
+     * {
+     *     return array('ROLE_USER');
+     * }
+     * </code>
+     *
+     * Alternatively, the roles might be stored on a ``roles`` property,
+     * and populated in any number of different ways when the user object
+     * is created.
+     *
+     * @return (Role|string)[] The user roles
+     */
+    public function getRoles()
+    {
+        return array($this->getRole()->getIntitule());
+    }
 
-    
+    /**
+     * Returns the password used to authenticate the user.
+     *
+     * This should be the encoded password. On authentication, a plain-text
+     * password will be salted, encoded, and then compared to this value.
+     *
+     * @return string The password
+     */
+    public function getPassword()
+    {
+        return $this->motdepasse;
+    }
+
+    /**
+     * Returns the salt that was originally used to encode the password.
+     *
+     * This can return null if the password was not encoded using a salt.
+     *
+     * @return string|null The salt
+     */
+    public function getSalt()
+    {
+        // TODO: Implement getSalt() method.
+    }
+
+    /**
+     * Returns the username used to authenticate the user.
+     *
+     * @return string The username
+     */
+    public function getUsername()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Removes sensitive data from the user.
+     *
+     * This is important if, at any given point, sensitive information like
+     * the plain-text password is stored on this object.
+     */
+    public function eraseCredentials(){}
 }
