f246daeea05592741c0278ee14eadfd3006785a0
ThomasBeWeb <camot81@gmail.com>
2018-10-12 04:25:56 +0200

    StartCharts

---
+++ public/build/js/add.js
@@ -0,0 +1,43 @@
+/*Ajoute une liste de reponses en BDD*/
+$('#addQuest').submit(function (e) {
+    e.preventDefault(e);
+
+    var data = {
+        "1": $("#quest1").val(),
+        "2": $("#quest2").val(),
+        "3": $("#quest3").val(),
+        "4": $("#quest4").val(),
+        "5": $("#quest5").val(),
+        "6": $("#quest6").val(),
+        "7": $("#quest7").val(),
+        "8": $("#quest8").val(),
+        "9": $("#quest9").val(),
+        "10": $("#quest10").val(),
+        "11": $("#quest11").val(),
+        "12": $("#quest12").val(),
+        "13": $("#quest13").val(),
+        "14": $("#quest14").val(),
+        "15": $("#quest15").val(),
+        "16": $("#quest16").val(),
+        "17": $("#quest17").val(),
+        "18": $("#quest18").val(),
+        "19": $("#quest19").val()
+    };
+
+    console.log(data);
+
+    $.ajax({
+        url : "http://esperluette/add",
+        type : "POST",
+        
+        data : data,
+        
+        success : function(donne){
+            console.log(donne);
+        },
+        
+        error : function (xhr, status, error) {
+            console.log(xhr.responseText)
+        }
+    });
+})
